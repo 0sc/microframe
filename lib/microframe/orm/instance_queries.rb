c5981d5a0e90afad49b7b9f76783db3115b8199b
Osmond <osmond.oranagwa@andela.com>
2015-12-15 17:19:27 +0000

    Add common queries. Create queries for instance method

---
+++ lib/microframe/orm/instance_queries.rb
@@ -0,0 +1,41 @@
+module Microframe
+  module ORM
+    module InstanceQueries
+      def save
+        @save_queryset["id"] = id
+        keys = @save_queryset.keys.join(", ")
+        values = @save_queryset.values
+        @save_queryset = nil
+        placeholders = Array.new(values.size, "?").join(", ")
+        result = Microframe::ORM::Connection.connect.execute("REPLACE INTO #{table_name} (#{keys}) VALUES (#{placeholders})", values)
+        result ? self.class.last : self
+      end
+
+      def update(options = {})
+        @save_queryset ||= {}
+        @save_queryset.merge(options)
+        save
+      end
+      # def order
+      # end
+
+      # def limit
+      # end
+
+      # def offset
+      # end
+
+      # def update
+      # end
+
+      def destroy
+        query = "DELETE FROM #{table_name} WHERE id =#{id}"
+          execute(query)
+          self
+      end
+
+      # def group
+      # end
+    end
+  end
+end
