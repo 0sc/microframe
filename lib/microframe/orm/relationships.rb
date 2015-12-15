802f62957efafebd4a752f5e710eae3cae96f33a
Osmond <osmond.oranagwa@andela.com>
2015-12-15 20:16:46 +0000

    Add class queries and methods for relationships

---
+++ lib/microframe/orm/relationships.rb
@@ -0,0 +1,28 @@
+module Microframe
+  module ORM
+    module Relationships
+
+      def has_many(model, options = {})
+        model = model.to_s
+        define_method(model) do
+          model_class = model[0..-2]
+          options[:foreign_key] ||= self.class.to_s.downcase + "_id"
+          Module.const_get(model_class.capitalize).where(options[:foreign_key] => id )
+        end
+      end
+
+      def belongs_to(model, options = {})
+        model = model.to_s
+        define_method(model) do
+          options[:foreign_key] ||= "id"
+          model_id = send((model + "_id").to_sym)
+          Module.const_get(model.capitalize).where(options[:foreign_key] => model_id )
+        end
+      end
+
+      # def validates
+      #   "With love from validates to"
+      # end
+    end
+  end
+end
