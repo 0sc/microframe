f9f0df02160aaa8ad5c9c9d3027a43c4ea2752fa
Osmond <osmond.oranagwa@andela.com>
2015-12-16 01:47:28 +0000

    Create generators for models and views

--- lib/microframe/application/generators/generator.rb
+++ lib/microframe/application/generators/generator.rb
@@ -5,13 +5,32 @@ module Microframe
     attr_reader :app_name, :type, :name, :xtras
 
     def self.source_root
-      __dir__
+      File.join(__dir__, "samples")
+    end
+
+    def target_root
+      "app/"
     end
 
     desc "Microframe new NAME", "Creates a new Microframe project called NAME"
     def new(name)
       @app_name = name
-      directory("samples/app_sample", "#{app_name}")
+      directory("app_sample", "#{app_name}")
+    end
+
+    def generate(type, name, *xtras)
+      @type = type.downcase
+      @name = name.downcase
+      @xtras = xtras
+
+      if type == "controller"
+        template("sample_controller.tt", File.join(target_root, "controllers", "#{name}_controller.rb"))
+        generate("view", name)
+      elsif type == "model"
+        template("sample_model.tt", File.join(target_root, "models", "#{name}.rb"))
+      elsif type == "view"
+        directory("folder", File.join(target_root, "views", "name"))
+      end
     end
   end
 end
