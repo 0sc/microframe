45fb090952d38aca6f8e11f061a41b190f9aa47c
Osmond <osmond.oranagwa@andela.com>
2015-12-16 01:47:28 +0000

    Create generators for models and views

--- lib/microframe.rb
+++ lib/microframe.rb
@@ -1,4 +1,4 @@
 require "microframe/version"
 require File.join(__dir__, "microframe", "application", "application")
 require File.join(__dir__, "microframe", "orm", "connection")
-require File.join(__dir__, "microframe", "orm", "connection")
+require File.join(__dir__, "microframe", "orm", "base")
