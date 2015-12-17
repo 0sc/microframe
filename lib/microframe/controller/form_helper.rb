552879fecb47c6c4b947c5cc9e48019f515b633d
Osmond <osmond.oranagwa@andela.com>
2015-12-17 23:30:05 +0000

    Fix bugs detected while creating sample app. Create Queryset class for more effcient chaining of queries. Improve form_helper. Add redirect feature

--- lib/microframe/controller/form_helper.rb
+++ lib/microframe/controller/form_helper.rb
@@ -1,20 +1,19 @@
 module Microframe
   class FormHelper
     attr_reader :target, :target_id, :form_started,:target_name
-    attr_accessor :link
 
-    def initialize (target, link)
+    def initialize (target, target_link)
       @target = target
       @target_name = target.class.to_s.downcase
-      @link = link
+      @link = target_link
       @form_started = false
     end
 
     def start_form
       @form_started = true
       @target_id = target.id ? target.id : nil
-      link ||= "/#{target_name}s/#{target_id || ""}"
-      "<form action='#{link}' method='post'>"
+      @link ||= "/#{target_name}s/#{target_id || ""}"
+      "<form action='#{@link}' method='post'>"
     end
 
     def label(name)
@@ -22,11 +21,11 @@ module Microframe
     end
 
     def text_area(name)
-      gatekeeper "<textarea name = '#{target_name}[name]'>#{target.send(name)}</textfield>"
+      gatekeeper "<textarea name = '#{target_name}[#{name}]'>#{target.send(name)}</textarea>"
     end
 
     def text_field(name)
-      gatekeeper "<input type = 'text' name = '#{target_name}[name]' value = '#{target.send(name)}'/>"
+      gatekeeper "<input type = 'text' name = '#{target_name}[#{name}]' value = '#{target.send(name)}'/>"
     end
 
     def submit
@@ -37,7 +36,15 @@ module Microframe
       gatekeeper output
     end
 
-    def checkbox
+    def check_box(name, val=nil)
+      val ||= target.send(name)
+      gatekeeper "<input type='checkbox' name='#{target_name}[#{name}]' checked = '#{val}' value = 'true'/>"
+    end
+
+    def hidden(val)
+      name = val.class.to_s.downcase
+      val = val.id
+      gatekeeper "<input type='hidden' name='#{name}_id' value = '#{val}' />"
     end
 
     def gatekeeper(output)
