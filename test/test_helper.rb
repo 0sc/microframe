c9bb1afe228267ec3f7284bdb3c779bc67672de0
Osmond <osmond.oranagwa@andela.com>
2015-12-18 21:17:56 +0000

    Add unit test for mapper class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'microframe'
 
+path = File.join(__dir__,"..", "lib", "microframe")
+
+Dir[File.join(path, "**")].each{ |file| $LOAD_PATH << file  }
+$LOAD_PATH << File.expand_path("unit_tests/", __dir__)
 require 'minitest/autorun'
+
+SampleRoutesWithPlaceholders =  {"PUT"=>{"/lists/:list_id/items/:id"=>{:controller=>"items", :action=>"update"}},
+ "DELETE"=>
+  {"/lists/:list_id/items/:id"=>{:controller=>"items", :action=>"destroy"}}}
+
+SampleRoutesWithOptionals =  {"GET"=>
+  {"/index/:id(/:di(/:ii(/params/:bb)))"=>
+    {:controller=>"welcome", :action=>"articles"}}}
+
+SampleRoutes = {"GET"=>
+  {"/lists/new"=>{:controller=>"lists", :action=>"new"},
+   "/lists/:id"=>{:controller=>"lists", :action=>"show"}},
+ "PATCH"=>{"/lists/:id"=>{:controller=>"lists", :action=>"update"}}}
